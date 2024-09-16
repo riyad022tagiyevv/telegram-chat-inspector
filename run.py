437d64a6b552c2c90f67cf62de3d938326f8f438
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2024-09-17 00:59:00 +0400

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,8 @@
-import os
-
 from bot import create_bot
 
-token = os.environ.get("BOT_TOKEN")
+token = "7246915007:AAHa_Pn-_pf3jsnFL_sUeFBMcMAIblJ-gjk"
 bot = create_bot(token)
 
 if __name__ == "__main__":
     bot.start_polling(1.0)
     bot.idle()
-    
